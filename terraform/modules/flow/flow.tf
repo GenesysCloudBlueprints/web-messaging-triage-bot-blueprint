a1448a926d2d652a151bcd837842e38e8635e7fe
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2022-12-05 13:27:15 +0800

    Update based on feedback

--- terraform/modules/flow/flow.tf
+++ terraform/modules/flow/flow.tf
@@ -1,5 +1,6 @@
 resource "genesyscloud_flow" "inbound_message_flow" {
   filepath = "${path.module}/web-messaging-triage-bot.yaml"
+  file_content_hash = filesha256("${path.module}/web-messaging-triage-bot.yaml")
   substitutions = {
     flow_name             = "web-messaging-triage-bot-flow"
     default_language      = "en-us"
@@ -11,6 +12,7 @@ resource "genesyscloud_flow" "inbound_message_flow" {
 
 resource "genesyscloud_flow" "bot_flow" {
   filepath = "${path.module}/quick-response-bot.yaml"
+  file_content_hash = filesha256("${path.module}/quick-response-bot.yaml")
   substitutions = {
     flow_name           = "web-messaging-quick-response-bot"
     default_language    = "en-us"
