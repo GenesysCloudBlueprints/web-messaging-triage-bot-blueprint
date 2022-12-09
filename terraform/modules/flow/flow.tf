cdd559a677d28f53b8249e52cb1f50b0ee27bd7c
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2022-12-09 12:29:46 +0800

    Updates on flow module

--- terraform/modules/flow/flow.tf
+++ terraform/modules/flow/flow.tf
@@ -3,11 +3,13 @@ resource "genesyscloud_flow" "inbound_message_flow" {
   file_content_hash = filesha256("${path.module}/web-messaging-triage-bot.yaml")
   substitutions = {
     flow_name             = "web-messaging-triage-bot-flow"
+    division              = "Home"
     default_language      = "en-us"
     queue_name            = "web-messaging-triage-bot-queue"
     data_action_category  = var.data_action_category
     data_action_name      = var.data_action_name
   }
+  depends_on = [genesyscloud_flow.bot_flow]
 }
 
 resource "genesyscloud_flow" "bot_flow" {
@@ -15,6 +17,7 @@ resource "genesyscloud_flow" "bot_flow" {
   file_content_hash = filesha256("${path.module}/quick-response-bot.yaml")
   substitutions = {
     flow_name           = "web-messaging-quick-response-bot"
+    division            = "Home"
     default_language    = "en-us"
   }
 }
